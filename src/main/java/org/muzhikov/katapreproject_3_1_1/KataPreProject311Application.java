5f8092111dc6328b1bff2a6eaced330ad7809888
Maxim-Muzhikov <muzikov.maxim@gmail.com>
2024-10-26 11:05:15 +0400

    Initial Commit

---
+++ src/main/java/org/muzhikov/katapreproject_3_1_1/KataPreProject311Application.java
@@ -0,0 +1,13 @@
+package org.muzhikov.katapreproject_3_1_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KataPreProject311Application {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(KataPreProject311Application.class, args);
+	}
+	
+}
---
+++ src/test/java/org/muzhikov/katapreproject_3_1_1/KataPreProject311ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.muzhikov.katapreproject_3_1_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KataPreProject311ApplicationTests {
+	
+	@Test
+	void contextLoads() {
+	}
+	
+}
